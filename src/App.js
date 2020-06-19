50b0368abee2ac7aba1b1cac92212e86d36827d8
Chaitanya Raj <chaitanyarajdixit@gmail.com>
2020-06-19 21:50:23 +0530

    basic component

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [bin, setBin] = useState("");
+  const [dec, setDec] = useState("");
+
+  const convert = () => {
+    setDec(parseInt(bin, 2));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="binary">
+        <label htmlFor="bin">Binary</label>
+        <input
+          type="text"
+          name="bin"
+          id="bin"
+          maxLength="8"
+          value={bin}
+          onChange={(e) => {
+            setBin(e.target.value);
+          }}
+        />
+      </div>
+      <button onClick={convert}>Convert</button>
+      <div className="decimal">
+        <label htmlFor="dec">Decimal</label>
+        <input
+          type="text"
+          name="dec"
+          id="dec"
+          value={dec}
+          onChange={(e) => {
+            setDec(e.target.value);
+          }}
+        />
+      </div>
     </div>
   );
 }
